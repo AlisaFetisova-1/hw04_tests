f17c2a860343df0cbd4aeca755e47c497c9a2367
Алиса Фетисова <Fetisova.alisa2002@yandex.ru>
2023-01-12 18:06:58 +0300

    05

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -34,13 +34,13 @@ class PostPagesTests(TestCase):
         templates_pages_names = {
             reverse('posts:index'): 'posts/index.html',
             reverse('posts:group',
-                kwargs={'slug': self.group.slug}): 'posts/group_list.html',
+                    kwargs={'slug': self.group.slug}): 'posts/group_list.html',
             reverse('posts:profile',
-                kwargs={'username': self.user.username}): 'posts/profile.html',
+                    kwargs={'username': self.user.username}): 'posts/profile.html',
             reverse('posts:post_detail',
-                kwargs={'post_id': self.post.pk}): 'posts/post_detail.html',
+                    kwargs={'post_id': self.post.pk}): 'posts/post_detail.html',
             reverse('posts:post_edit',
-                kwargs={'post_id': self.post.id}): 'posts/create_post.html',
+                    kwargs={'post_id': self.post.id}): 'posts/create_post.html',
             reverse('posts:post_create'): 'posts/create_post.html',
         }
 
